19349ea4cddc351338774c9ff8f57f72d1409f7b
naman1028 <116903638+naman1028@users.noreply.github.com>
2024-04-26 20:56:41 +0530

    test

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull namanjio/simple-python-flask-app:v1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 namanjio/simple-python-flask-app:v1
+docker run -d -p 5000:5000 namanjio/simple-python-flask-app:v1   
